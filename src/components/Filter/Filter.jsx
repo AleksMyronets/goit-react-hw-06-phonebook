48094aef16cadb75e4094de00e055f8974f87133
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-06-12 21:40:10 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,15 +1,21 @@
-import PropTypes from 'prop-types'
-import { InputFilter } from "./Filter.styled"
+import { InputFilter } from './Filter.styled'
+import { useSelector } from 'react-redux'
+import { addValue } from 'redux/sliceFilter'
+import { useDispatch } from 'react-redux'
 
-export const Filter = ({ value, onChange }) => {
+export const Filter = () => {
+    const filterValue = useSelector(state => state.filter);
+    const dispatch = useDispatch;
+
+    const changeFilter = event => {
+      dispatch(addValue(event.currentTarget.value));
+    };
+
     return (
-          <label>
-              <InputFilter type="text" placeholder="Find contacts by name" value={value} onChange={onChange}/>
-        </label>
-    )
-} 
+      <label>
+        Find contacts by name
+        <InputFilter type="text" value={filterValue} onChange={changeFilter} />
+      </label>
+    );
+}
 
-Filter.propTypes = {
-    value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired
-};
