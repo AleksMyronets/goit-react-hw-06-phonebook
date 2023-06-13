0398ab299a6341a75d758f8d6188285a95f74279
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-06-13 22:41:31 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,21 +1,21 @@
-import { InputFilter } from './Filter.styled'
-import { useSelector } from 'react-redux'
-import { addValue } from 'redux/sliceFilter'
-import { useDispatch } from 'react-redux'
+import { InputFilter } from './Filter.styled';
+import { useSelector } from 'react-redux';
+import { addValue } from 'redux/sliceFilter';
+import { useDispatch } from 'react-redux';
 
 export const Filter = () => {
-    const filterValue = useSelector(state => state.filter);
-    const dispatch = useDispatch;
+  const filterValue = useSelector(state => state.filter);
+  const dispatch = useDispatch;
 
-    const changeFilter = event => {
-      dispatch(addValue(event.currentTarget.value));
-    };
+  const changeFilter = event => {
+    dispatch(addValue(event.currentTarget.value));
+  };
 
-    return (
-      <label>
-        Find contacts by name
-        <InputFilter type="text" value={filterValue} onChange={changeFilter} />
-      </label>
-    );
-}
+  return (
+    <label>
+      Find contacts by name
+      <InputFilter type="text" value={filterValue} onChange={changeFilter} />
+    </label>
+  );
+};
 
