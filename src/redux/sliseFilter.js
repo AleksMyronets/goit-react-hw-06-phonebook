551a84fff2d8237025680833367c7d5bed8c443b
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-06-14 12:17:56 +0300

    Update sliseFilter.js

--- src/redux/sliseFilter.js
+++ src/redux/sliseFilter.js
@@ -4,7 +4,7 @@ const filterSlice = createSlice({
   name: 'filter',
   initialState: '',
   reducers: {
-    addValue: (state, { payload }) => {
+    setFilter: (state, { payload }) => {
       return (state = payload);
     },
   },
